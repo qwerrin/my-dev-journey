1c4571ecac096af43919fe1e7b3e1676727a4a83
qwerrin <springephemeral1123@gmail.com>
2026-05-16 08:59:31 +0900

    refactor: JSONファイルからログを読み込む形式に変更

--- app.js
+++ app.js
@@ -1,39 +1,24 @@
-const taskInput = document.getElementById('taskInput');
-const addBtn = document.getElementById('addBtn');
-const logList = document.getElementById('logList');
-
+const logList = document.getElementById('logList')
 // 1. ページ読み込み時に保存されたデータを表示する
-window.addEventListener('load', () => {
-    const savedData = localStorage.getItem('myLogs');
-    if (savedData) {
-        const logs = JSON.parse(savedData);
+window.addEventListener('load', async () => {
+    const response = await fetch("./logs.json");
+    if (response.ok) {
+        const logs = await response.json();
         logs.forEach(text => addToList(text));
     }
 });
 
 // 2. リストに追加する共通の関数
-function addToList(text) {
+function addToList({ date, content }) {
     const li = document.createElement('li');
-    li.textContent = text;
+    li.innerHTML = `
+        <p class="date">${date}</p>
+        <span>${content}</span>
+    `;
     logList.appendChild(li);
+
 }
 
-// 3. データを保存する関数
-function saveToLocal() {
-    const logs = [];
-    document.querySelectorAll('#logList li').forEach(li => {
-        // "✅" の絵文字を除いてテキストだけ保存（CSSのbeforeで付けている場合）
-        logs.push(li.textContent);
-    });
-    localStorage.setItem('myLogs', JSON.stringify(logs));
-}
 
-// ボタンクリック時の処理
-addBtn.addEventListener('click', () => {
-    const text = taskInput.value;
-    if (text !== "") {
-        addToList(text);
-        saveToLocal(); // 保存！
-        taskInput.value = "";
-    }
-});
+
+
